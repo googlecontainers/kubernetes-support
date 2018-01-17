ad3ab8e4a4a9279fc263f53591d005077b54be71
chenzhenyang <czy_1@qq.com>
2018-01-17 17:17:38 +0800

    jsonpath.sh

--- pull.sh
+++ pull.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# curl curl https://api.github.com/orgs/googlecontainers/repos?access_token=760cc6977d6631e8ef06cf5dbb8374ce1c43aa28
-cd $1
-imageRepos=`curl https://api.github.com/orgs/googlecontainers/repos?access_token=760cc6977d6631e8ef06cf5dbb8374ce1c43aa28 | jq -r '.[].name'`
+# curl  https://api.github.com/orgs/googlecontainers/repos?access_token=760cc6977d6631e8ef06cf5dbb8374ce1c43aa28
+#imageRepos=`curl https://api.github.com/orgs/googlecontainers/repos?access_token=760cc6977d6631e8ef06cf5dbb8374ce1c43aa28 | jq -r '.[].name'`
+
+curl -LO https://raw.githubusercontent.com/mclarkson/JSONPath.sh/master/JSONPath.sh
+chmod +x JSONPath.sh
+mv -f JSONPath.sh ~bin
+
+imageRepos=`curl https://api.github.com/orgs/googlecontainers/repos?access_token=0c465edc7c178cd73fdf1d3683b061eebfea1aac | JSONPath.sh '.[*].name' -b`
+
 for repo in $imageRepos;
 do
   git clone "https://github.com/googlecontainers/"$repo".git"
